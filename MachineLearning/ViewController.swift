5357f6e3b84781034c0aaf55a482eec96e28d15c
Tolga Sarikaya <113526329+TolgaSarikayaa@users.noreply.github.com>
2023-12-31 11:24:07 +0100

    first commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Tolga Sarikaya on 31.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
